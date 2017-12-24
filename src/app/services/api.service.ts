2666c4c99d13782c11404ca2684a4a2f98a22490
Stephen Wright <sjw948@gmail.com>
2017-12-24 20:05:57 +0000

    update ngIf to avoid length queries failing + apiService url call params

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,7 +9,6 @@ export class ApiService {
 
   baseUrl: string = environment.apiUrl;
   authorization: String = environment.authorization;
-  adminAuthorization: String = environment.adminAuthorization;
 
   constructor(
     private http: HttpClient,
@@ -18,7 +17,7 @@ export class ApiService {
 
   leagues = {
     get: (id) => {
-      const callParams = {type: 'get', url: `/leagues/${id}`}
+      const callParams = {type: 'get', url: `/admin/leagues/${id}`}
       return this.apiCall(callParams);
     },
 
@@ -28,51 +27,51 @@ export class ApiService {
     },
 
     create: (league) => {
-      const callParams = {type: 'post', url: '/leagues', body: league}
+      const callParams = {type: 'post', url: '/admin/leagues', body: league}
       return this.apiCall(callParams);
     },
 
     delete: (id) => {
-      const callParams = {type: 'delete', url: `/leagues/${id}`}
+      const callParams = {type: 'delete', url: `/admin/leagues/${id}`}
       return this.apiCall(callParams);
     },
 
     update: (league) => {
-      const callParams = {type: 'put', url: `/leagues/${league._id}`, body: league}
+      const callParams = {type: 'put', url: `/admin/leagues/${league._id}`, body: league}
       return this.apiCall(callParams);
     },
   }
 
   teams = {
     get: (id) => {
-      const callParams = {type: 'get', url: `/teams/${id}`}
+      const callParams = {type: 'get', url: `/admin/teams/${id}`}
       return this.apiCall(callParams);
     },
 
     getAll: () => {
-      const callParams = {type: 'get', url: '/admin/teams'}
+      const callParams = {type: 'get', url: '/admin/admin/teams'}
       return this.apiCall(callParams);
     },
 
     create: (team) => {
-      const callParams = {type: 'post', url: '/teams', body: team}
+      const callParams = {type: 'post', url: '/admin/teams', body: team}
       return this.apiCall(callParams);
     },
 
     delete: (id) => {
-      const callParams = {type: 'delete', url: `/teams/${id}`}
+      const callParams = {type: 'delete', url: `/admin/teams/${id}`}
       return this.apiCall(callParams);
     },
 
     update: (team) => {
-      const callParams = {type: 'put', url: `/teams/${team._id}`, body: team}
+      const callParams = {type: 'put', url: `/admin/teams/${team._id}`, body: team}
       return this.apiCall(callParams);
     },
   }
 
   fixtures = {
     get: (id) => {
-      const callParams = {type: 'get', url: `/fixtures/${id}`}
+      const callParams = {type: 'get', url: `/admin/fixtures/${id}`}
       return this.apiCall(callParams);
     },
 
@@ -82,24 +81,24 @@ export class ApiService {
     },
 
     create: (fixture) => {
-      const callParams = {type: 'post', url: '/fixtures', body: fixture}
+      const callParams = {type: 'post', url: '/admin/fixtures', body: fixture}
       return this.apiCall(callParams);
     },
 
     delete: (id) => {
-      const callParams = {type: 'delete', url: `/fixtures/${id}`}
+      const callParams = {type: 'delete', url: `/admin/fixtures/${id}`}
       return this.apiCall(callParams);
     },
 
     update: (fixture) => {
-      const callParams = {type: 'put', url: `/fixtures/${fixture._id}`, body: fixture}
+      const callParams = {type: 'put', url: `/admin/fixtures/${fixture._id}`, body: fixture}
       return this.apiCall(callParams);
     },
   }
 
   users = {
     get: (id) => {
-      const callParams = {type: 'get', url: `/private/users/${id}`}
+      const callParams = {type: 'get', url: `/admin/users/${id}`}
       return this.apiCall(callParams);
     },
 
@@ -109,22 +108,17 @@ export class ApiService {
     },
 
     create: (user) => {
-      const callParams = {type: 'post', url: '/users', body: user}
+      const callParams = {type: 'post', url: '/admin/users', body: user}
       return this.apiCall(callParams);
     },
 
     delete: (id) => {
-      const callParams = {type: 'delete', url: `/users/${id}`}
+      const callParams = {type: 'delete', url: `/admin/users/${id}`}
       return this.apiCall(callParams);
     },
 
     update: (user) => {
-      const callParams = {type: 'put', url: `/users/${user._id}`, body: user}
-      return this.apiCall(callParams);
-    },
-
-    authenticate: (user) => {
-      const callParams = {type: 'post', url: '/users/authenticate', body: user}
+      const callParams = {type: 'put', url: `/admin/users/${user._id}`, body: user}
       return this.apiCall(callParams);
     },
 
@@ -134,16 +128,9 @@ export class ApiService {
     },
   }
 
-  website = {
-    contactForm: (message) => {
-      const callParams = {type: 'post', url: '/website/contact-form', body: message}
-      return this.apiCall(callParams);
-    },
-  }
-
   apiCall(callParams) {
     const jwt = localStorage.getItem('token');
-    let headers = new HttpHeaders({'Authorization': `${callParams.authorization || this.authorization}`});
+    let headers = new HttpHeaders({'Authorization': `${this.authorization}`});
     if(jwt) {
       headers = headers.set('token', jwt);
     }
